4909bf269fe0e50587db8f8b3facb900afa2e00f
Artem Kivachuk <mamokosf@gmail.com>
2019-02-17 14:41:34 +0300

    first solving

--- StringExtension.cs
+++ StringExtension.cs
@@ -6,8 +6,20 @@ namespace PadawansTask7
     {
         public static void OrderStringsByLength(string[] array)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (array==null)
+                throw new ArgumentNullException();
+            for(int i = 1; i < array.Length; i++)
+            {
+                int j = i;
+                while (j > 0 && array[j-1].Length > array[j].Length)
+                {
+                    string temp = array[j]; 
+                    array[j] = array[j-1];
+                    array[j-1] = temp;
+                    j--;
+                }
+            }
+            //2 1 4 5 6
         }
     }
 }
